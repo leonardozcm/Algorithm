661fac8e6ccabde3dad9d5dc2eca1b858482ec8e
leonardozcm <597714572@qq.com>
2020-10-02 16:31:43 +0800

    em

--- build/CMakeFiles/CCFAnswer.dir/DependInfo.cmake
+++ build/CMakeFiles/CCFAnswer.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/chriskafka/project/Algorithm/main.cpp" "/home/chriskafka/project/Algorithm/build/CMakeFiles/CCFAnswer.dir/main.cpp.o"
+  "/home/chriskafa/MyProjects/Algorithm/main.cpp" "/home/chriskafa/MyProjects/Algorithm/build/CMakeFiles/CCFAnswer.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
